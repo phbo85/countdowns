9cdfbae0cbddc4a7ceea2474c207bbc1d47cb386
Philipp <p.bochmann@googlemail.com>
2024-12-15 13:54:36 +0100

    add empty message

--- src/components/media-list.tsx
+++ src/components/media-list.tsx
@@ -2,12 +2,22 @@
 
 import MediaTile from "@/components/media-tile";
 import { useMediaStore } from "@/stores/media-store";
+import { Card, CardHeader, CardTitle } from "./ui/card";
 
 export default function MediaList() {
   const { selectedMedia, removeMedia } = useMediaStore();
 
   return (
     <div className="flex flex-wrap gap-4">
+      {selectedMedia.length === 0 && (
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              Nothing tracked yet, search for a movie or tv show to add
+            </CardTitle>
+          </CardHeader>
+        </Card>
+      )}
       {selectedMedia.map((media) => (
         <MediaTile
           key={media.id}
